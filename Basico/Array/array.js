b17cfe5f28ab48ddd1a708305371a249cf0659c2
andrevale99 <andrefvale@gmail.com>
2023-02-10 10:03:57 -0300

    Outras funções para manipulação de arrays

--- Basico/Array/array.js
+++ Basico/Array/array.js
@@ -16,9 +16,26 @@ function show_notas(entrada)
  
     alunos.forEach(adicionar);
     console.log("4: "+alunos);
+
+    alunos.forEach(show_wit_foreach);
+
+    alunos.forEach(e => console.log("6: " + e));
+
+    var new_alunos = alunos.map(new_array_with_map);
+    new_alunos.forEach(e=>console.log("7: " +e));
 }
 
 function adicionar(valor, idx, array)
 {
     array[idx] = valor + idx;
+}
+
+function show_wit_foreach(valor)
+{
+    console.log("5: " + valor);
+}
+
+function new_array_with_map(valor, idx, array)
+{
+    return valor - array[idx];
 }
